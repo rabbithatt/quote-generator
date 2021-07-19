68cf97feac061a911173c6c8f20a0ee78bd0e1ed
rabbithatt <rabbithat.red@gmail.com>
2021-07-19 17:02:09 -0300

    commit to main

---
+++ script.js
@@ -0,0 +1,59 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+let apiQuotes = [];
+
+// Show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading 
+function complete() {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
+// Show new Quote
+function newQuote() {
+    loading();
+    // Pick a random quto from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+    authorText.textContent = quote.author ? quote.author : 'Unknown' ;
+    if(quote.text.length > 50){
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    // Set quote, hide loader
+    quoteText.textContent = quote.text;
+    complete();
+}
+// Get quotes from API
+async function getQuotes() {
+    loading();
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch(error) {
+        // Catch error here
+    }
+}
+// Tweet quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+//Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+// On load
+getQuotes();
